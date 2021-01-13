0660a63ffc1d2caea0989b6b5412587e4db748d3
raystatic <rahul9650ray@gmail.com>
2021-01-13 16:00:18 +0530

    header logo added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      {/* Header */}
+      <Header/>
+      {/* Blog */}
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <img className="header__logo"
+                src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png"
+                alt=""
+            />
+        </div>
+    )
+}
+
+export default Header
